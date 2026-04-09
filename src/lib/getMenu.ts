23486fd4a0665ee09e742955cfb6b043206d6484
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2026-04-09 20:27:26 +0200

    feat(menu): update dates for next week’s menu

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,9 +1,9 @@
 import type { Menu } from './types/menu'
 
 const MENU: Menu = {
-  '2026-04-07': ['ugnsbakad-lax'],
-  '2026-04-08': ['pokebowl'],
-  '2026-04-09': ['spenat-och-fetaostpaj']
+  '2026-04-14': ['ugnsbakad-lax'],
+  '2026-04-15': ['pokebowl'],
+  '2026-04-16': ['spenat-och-fetaostpaj']
 }
 
 export function getMenu(): Menu {
