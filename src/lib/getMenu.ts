9f208633a0e852cc03b4f4fb42af5c3a0935b584
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2026-05-05 12:44:02 +0200

    feat: change menu dates

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,7 +1,7 @@
 import type { Menu } from './types/menu'
 
 const MENU: Menu = {
-  '2026-05-04': ['currykyckling', 'currykyckling'],
+  '2026-05-05': ['currykyckling', 'currykyckling'],
   '2026-05-06': ['halloumi-potatis-tzatziki'],
   '2026-06-07': ['gnocchi-köttfärssås', 'gnocchi-köttfärssås']
 }
