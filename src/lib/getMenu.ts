f9cef1e2ff620f2539bab33e69935cd0178532c2
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2026-06-29 18:34:42 +0200

    menu: formatting

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -4,7 +4,7 @@ const MENU: Menu = {
   '2026-07-01': ['chicken-tikka-masala', 'chicken-tikka-masala'],
   '2026-07-03': ['laxokado'],
   '2026-07-04': ['svamprisotto'],
-  '2026-07-05': ['chana-daal', 'krämig-lax'],
+  '2026-07-05': ['chana-daal', 'krämig-lax']
 }
 
 export function getMenu(): Menu {
