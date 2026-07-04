4ee743444444c5014cca927e0b82ea5bf34b1ad6
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2026-07-04 13:04:22 +0200

    menu: plan week

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,10 +1,10 @@
 import type { Menu } from './types/menu'
 
 const MENU: Menu = {
-  '2026-07-01': ['chicken-tikka-masala', 'chicken-tikka-masala'],
-  '2026-07-03': ['laxokado'],
-  '2026-07-04': ['svamprisotto'],
-  '2026-07-05': ['chana-daal', 'krämig-lax']
+  '2026-07-06': ['laxokado'],
+  '2026-07-07': ['tofu-stroganoff', 'tofu-stroganoff'],
+  '2026-07-09': ['chana-daal'],
+  '2026-07-10': ['svamprisotto'],
 }
 
 export function getMenu(): Menu {
