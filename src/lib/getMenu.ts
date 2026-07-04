acfe46f7109c8ad6b6a3127fc70eec748123a0d9
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2026-07-04 13:05:55 +0200

    menu: fix formatting

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -4,7 +4,7 @@ const MENU: Menu = {
   '2026-07-06': ['laxokado'],
   '2026-07-07': ['tofu-stroganoff', 'tofu-stroganoff'],
   '2026-07-09': ['chana-daal'],
-  '2026-07-10': ['svamprisotto'],
+  '2026-07-10': ['svamprisotto']
 }
 
 export function getMenu(): Menu {
