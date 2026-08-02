2c56feecdf7b1e6d37abf66bcf08a7e1fa470523
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2026-08-02 20:58:35 +0200

    menu: plan week

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -5,7 +5,7 @@ const MENU: Menu = {
   '2026-08-04': ['färdig-risotto'],
   '2026-08-05': ['italiensk-kycklinggryta'],
   '2026-08-06': ['pokebowl'],
-  '2026-08-09': ['ugnsbakad-lax'],
+  '2026-08-09': ['ugnsbakad-lax']
 }
 
 export function getMenu(): Menu {
