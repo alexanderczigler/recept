c4a3c16d1239968e7e4925cb7d066d5fb24569d3
Alexander Czigler <3116043+alexanderczigler@users.noreply.github.com>
2026-09-10 18:36:06 +0200

    menu: plan

--- src/lib/getMenu.ts
+++ src/lib/getMenu.ts
@@ -1,11 +1,9 @@
 import type { Menu } from './types/menu'
 
 const MENU: Menu = {
-  '2026-08-03': ['gnocchi-köttfärssås'],
-  '2026-08-04': ['färdig-risotto'],
-  '2026-08-05': ['italiensk-kycklinggryta'],
-  '2026-08-06': ['pokebowl'],
-  '2026-08-09': ['ugnsbakad-lax']
+  '2026-09-12': ['gnocchi-köttfärssås'],
+  '2026-09-13': ['chana-daal'],
+  '2026-09-15': ['ugnsbakad-lax']
 }
 
 export function getMenu(): Menu {
